d453fa181fa7cb8eb742bacfa5f68832d724f742
pavel <lisovich.p@gmail.com>
2022-02-14 22:13:15 +0600

    ttt

---
+++ Index.js
@@ -0,0 +1,38 @@
+// 1) Дан массив с числами. Используя метод map к каждому
+// элементу массива добавьте 20 и запишите результат в новый
+// массив. Выведите в консоли новый массив. (max 5б)
+
+// let arraySecond = [0, 2, 3, 4, 5, 6, 7, 8, 9]
+//  const arrayDouble = arraySecond.map((num) => {
+//   return num + 20
+// })
+// console.log(arrayDouble);
+// }
+// console.log(obj.sayHello());
+
+// 4) Дан объект const obj = {green: 'зеленый', red: 'красный',
+// blue: 'голубой'}. С помощью цикла for-in выведите на
+// экран ключи и элементы этого объекта.(max 5б)
+
+// let obj = {green: 'зеленый', red: 'красный',blue: 'голубой'}
+//  for (variable in obj) {
+// console.log(variable + ' = ' + obj[variable] )
+// }
+
+// 5) Если юзер ввел Ваше имя, то в консоли вывести
+// "Приветствую, <Ваше имя>!". Если юзер ввел имя Насиба, в
+// консоли вывести "Приветствую, Насиба!". Иначе, вывести в
+// консоли "Я Вас не знаю :(".(max 5б)
+
+// function hello() {
+//   const userName = prompt(' Your name');
+//   console.log(userName);
+//   if (userName) {
+//     console.log(`Приветствую, ${userName}`);
+//     alert(`Приветствую, ${userName}`);
+//   } else {
+//     console.log('вы кто?');
+//     alert('вы кто?');
+//   }
+// }
+// hello();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ITAcademyExam
+# ITAcademyExam 
+lisovich.p@gmail.com
+ФИО - Лисовский Павел Павлович
+https://github.com/PavelLisovich/ITAcademyExam.git
